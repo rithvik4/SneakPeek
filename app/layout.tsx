ff8e11763c500701f9897e8e52637ccfd0742ed1
Bura Rithvik Kumar <105736916+rithvik4@users.noreply.github.com>
2026-07-22 17:42:47 +0530

    landing page animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Space_Grotesk } from "next/font/google";
+import { LandingIntroGate } from "@/app/components/Intro/LandingIntroGate";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${spaceGrotesk.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <LandingIntroGate>{children}</LandingIntroGate>
+      </body>
     </html>
   );
 }
